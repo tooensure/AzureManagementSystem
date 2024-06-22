72ce219d4516183b49d93d158ed0d921b56dcaf8
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-21 20:20:19 -0500

    feat(core): Add IBaseEntityOperations interface
    Add a new interface IBaseEntityOperations to define common CRUD operations for entities.

---
+++ src/AzureManagementSystem.Core/Interfaces/IBaseEntityOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Core.Interfaces
+{
+    public interface IBaseEntityOperations<T> where T : class
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T?> GetAsync(int id);
+        Task<bool> DeleteAsync(int id);
+        Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate,
+                                        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy);
+        Task<int> UpdateAsync(T entity);
+    }
+}
