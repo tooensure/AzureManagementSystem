596ca3190aefbb306be2711050dc5e2f1da6a5ff
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-21 20:21:48 -0500

    feat(core): Introduce IRepository interface
    Introduce a generic IRepository interface that extends IBaseEntityOperations to represent a repository for entities.

---
+++ src/AzureManagementSystem.Core/Interfaces/IRepositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Core.Interfaces.IRepositories
+{
+    public interface IRepository<T>: IBaseEntityOperations<T> where T : class
+    {
+    }
+}
