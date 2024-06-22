aa42bcea237f65970a1afc347bb3c522720df9a8
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-21 20:29:20 -0500

    feat(domain): Create GenericRepository base class
    Implement a generic GenericRepository base class that provides the foundation for repository implementations, inheriting from IRepository. This class serves as a blueprint for concrete repository implementations.

---
+++ src/AzureManagementSystem.Domain/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using AzureManagementSystem.Core.Interfaces.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Domain.Repositories
+{
+    public abstract class GenericRepository<T> : IRepository<T> where T : class
+    {
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
