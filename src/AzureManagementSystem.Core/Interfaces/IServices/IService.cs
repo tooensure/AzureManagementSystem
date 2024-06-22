50624fda98a6bc364b3f35a91bf108ba1b2e4ae1
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-21 20:24:17 -0500

    feat(core): Define IService interface
    Define a generic IService interface that extends IBaseEntityOperations to represent a service for entities.

---
+++ src/AzureManagementSystem.Core/Interfaces/IServices/IService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Core.Interfaces.IServices
+{
+    public interface IService<T> : IBaseEntityOperations<T> where T : class
+    {
+    }
+}
