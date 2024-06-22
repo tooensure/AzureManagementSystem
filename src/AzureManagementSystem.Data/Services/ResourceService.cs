57707e875b52202f801a91ea8d5719af81fd3fbc
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-21 20:28:26 -0500

    feat(data): Implement specific services
    Implement specific service classes for Resource, Role, and User by inheriting from GenericService and their respective interfaces. Inject the corresponding repositories during construction.

---
+++ src/AzureManagementSystem.Data/Services/ResourceService.cs
@@ -0,0 +1,19 @@
+﻿using AzureManagementSystem.Core.Interfaces.IRepositories;
+using AzureManagementSystem.Core.Interfaces.IServices;
+using AzureManagementSystem.Core.Models;
+using AzureManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Data.Services
+{
+    public class ResourceService : GenericService<Resource>, IResourceService
+    {
+        public ResourceService(IRepository<Resource> repository) : base(repository)
+        {
+        }
+    }
+}
