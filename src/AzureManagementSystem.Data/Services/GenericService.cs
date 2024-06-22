93c0dd4e5f5060190d4221173298ac0b09361892
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-21 20:25:32 -0500

    feat(data): Implement GenericService base class
    Implement a generic GenericService base class that provides the foundation for service implementations, inheriting from IService. It utilizes a dependency-injected IRepository to delegate CRUD operations.

---
+++ src/AzureManagementSystem.Data/Services/GenericService.cs
@@ -0,0 +1,62 @@
+﻿using AzureManagementSystem.Core.Interfaces.IRepositories;
+using AzureManagementSystem.Core.Interfaces.IServices;
+using AzureManagementSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Data.Services
+{
+    public abstract class GenericService<T> : IService<T> where T : class
+    {
+        private readonly IRepository<T> _repository;
+
+        protected GenericService(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
