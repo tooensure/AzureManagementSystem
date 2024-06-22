e540eac22f2d737bf259a0c94185471ebb6990c8
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-22 00:15:13 -0500

    Update

--- builds/resource-group.ps1
+++ builds/resource-group.ps1
@@ -1,1 +1,8 @@
-New-AzResourceGroup -Name 'myResourceGroup' -Location 'eastus'
+# Get the name of the resource group from the user
+$resourceGroupName = Read-Host "Enter the name of the resource group:"
+
+# Create the resource group
+New-AzResourceGroup -Name $resourceGroupName -Location "eastus"
+
+# Write to the console
+Write-Host "Resource group '$resourceGroupName' created successfully!"
