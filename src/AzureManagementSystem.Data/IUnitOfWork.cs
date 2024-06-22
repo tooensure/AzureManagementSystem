b07af5a9302000d3ccd6f65f7cfd85f9af7f4384
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-06-21 20:30:57 -0500

    Project updates

---
+++ src/AzureManagementSystem.Data/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using AzureManagementSystem.Core.Interfaces.IRepositories;
+using AzureManagementSystem.Core.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Data
+{
+    public interface IUnitOfWork: IDisposable
+    {
+
+        IResourceService Resources { get; }
+        IRoleService Roles { get; }
+        IUserService Users { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ src/AzureManagementSystem.Data/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using AzureManagementSystem.Core.Interfaces.IRepositories;
+using AzureManagementSystem.Core.Interfaces.IServices;
+using AzureManagementSystem.Data.Repositories;
+using AzureManagementSystem.Data.Services;
+using AzureManagementSystem.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureManagementSystem.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+
+        public UnitOfWork()
+        {
+
+            IResourcesRepository resources = new ResourcesRepository();
+            IUsersRepository users = new UsersRepository();
+            IRolesRepository roles = new RolesRepository();
+
+            Resources = new ResourceService(resources);
+            Roles = new RoleService(roles);
+            Users = new UserService(users);
+
+        }
+        public IResourceService Resources { get; }
+        public IRoleService Roles { get; }
+        public IUserService Users { get; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
